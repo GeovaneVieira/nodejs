162855b4f88aac3f056ecf0dce2fb0ceab53be51
Geovane Vieira <geovane.vieira@hqsplus.com.br>
2018-03-22 18:55:39 -0300

    . Continuação

--- livro-realtime/cap02/url_server.js
+++ livro-realtime/cap02/url_server.js
@@ -1,17 +1,18 @@
 var http = require('http');
-var http = require('url');
+var url = require('url');
 
-var server = http.createServer((request, response) => {
+var server = http.createServer(function(request, response) {
+    
     response.writeHead(200, {"Content-Type": "text/html; charset=utf-8" });
     response.write("<h1>Dados da QueryString</h1>");
-    var result = url.parse(request.url, true);
+    var result = url.parse(request.url, true, true);
 
     for (var key in result.query) {
-        response.write("<h2>"+key+"</h2>");
+        response.write("<h2>" + key +": " + result.query[key] + "</h2>");
     }
+    console.log(result);
 
 
-    
     response.end();
 });
 
